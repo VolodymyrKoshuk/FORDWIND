fc58bfa2f7a467f068d8aa3feac70b3277e759ac
Volodymyr Koshuk <vovokos2001@gmail.com>
2024-02-13 15:17:21 +0200

    Added providers for eks cluster

--- version.tf
+++ version.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
+    }
+    time = {
+      version = "~> 0.9"
+    }
+  }
+}
+
+
 provider "aws" {
   region = var.terr_region
 }
